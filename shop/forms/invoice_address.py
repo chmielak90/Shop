14bad7d9a28d10084d0532f210463cf7a908b31b
Michal Chmielewski <90.michal.chmielewski@gmail.com>
2017-12-06 00:57:49 +0100

    fix the csrf validation

--- shop/forms/invoice_address.py
+++ shop/forms/invoice_address.py
@@ -1,10 +1,20 @@
 from django import forms
 
-from shop.models import Invoice
+from shop.models import Address
 
 
-class InvoiceAddressForm(forms.ModelForm):
+class InvoiceAddressForm(forms.Form):
 
-    class Meta:
-        model = Invoice
-        fields = ('bill_address',)
+    CHOICE = []
+    addresses = Address.objects.all()
+    for address in addresses:
+        CHOICE.append((address.id, address))
+
+    CNT_ADD = {
+        'class': 'form-control col-sm-2',
+        'id': 'invoice_address'
+    }
+
+    bill_address = forms.CharField(label='Invoice address:',
+                                   widget=forms.Select(attrs=CNT_ADD,
+                                                       choices=CHOICE))
--- shop/forms/order_address.py
+++ shop/forms/order_address.py
@@ -1,10 +1,27 @@
 from django import forms
 
-from shop.models import Order
+from shop.models import Address
 
 
-class OrderAddressForm(forms.ModelForm):
+class OrderAddressForm(forms.Form):
 
-    class Meta:
-        model = Order
-        fields = ('comments', 'send_address')
+    CHOICE = []
+    addresses = Address.objects.all()
+    for address in addresses:
+        CHOICE.append((address.id, address))
+
+    CNT_COMM = {
+        'class': 'form-control col-sm-2',
+        'id': 'comm'
+    }
+
+    CNT_ADD = {
+        'class': 'form-control col-sm-2',
+        'id': 'send_address'
+    }
+
+    comments = forms.CharField(label='Comment to order:',
+                               widget=forms.Textarea(attrs=CNT_COMM))
+    send_address = forms.CharField(label='Send address:',
+                                   widget=forms.Select(attrs=CNT_ADD,
+                                                       choices=CHOICE))
