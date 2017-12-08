6f14f77c9e1461962b956b7ff547afcbc51d05dc
Michal Chmielewski <90.michal.chmielewski@gmail.com>
2017-12-08 22:33:16 +0100

    two forms combined

--- shop/forms/__init__.py
+++ shop/forms/__init__.py
@@ -1,6 +1,5 @@
 from .change_password import ChangePasswordForm
 from .contact_form import ContactForm
-from .invoice_address import InvoiceAddressForm
 from .login import LoginForm
 from .order_address import OrderAddressForm
 from .product_availability import ProductAvailabilityForm
--- shop/forms/order_address.py
+++ shop/forms/order_address.py
@@ -5,14 +5,9 @@ from shop.models import Address
 
 class OrderAddressForm(forms.Form):
 
-    CHOICE = []
-    addresses = Address.objects.all()
-    for address in addresses:
-        CHOICE.append((address.id, address))
-
     CNT_COMM = {
         'class': 'form-control col-sm-2',
-        'id': 'comm'
+        'id': 'comment'
     }
 
     CNT_ADD = {
@@ -20,8 +15,23 @@ class OrderAddressForm(forms.Form):
         'id': 'send_address'
     }
 
+    CNT_INV_ADD = {
+        'class': 'form-control col-sm-2',
+        'id': 'invoice_address'
+    }
+
     comments = forms.CharField(label='Comment to order:',
                                widget=forms.Textarea(attrs=CNT_COMM))
-    send_address = forms.CharField(label='Send address:',
-                                   widget=forms.Select(attrs=CNT_ADD,
-                                                       choices=CHOICE))
+    send_address = forms.CharField(label='Send address',
+                                   widget=forms.Select(attrs=CNT_ADD))
+    bill_address = forms.CharField(label='Bill address:',
+                                   widget=forms.Select(attrs=CNT_INV_ADD))
+
+    def __init__(self, user, *args, **kwargs):
+        choice = []
+        addresses = Address.objects.filter(user=user)
+        for address in addresses:
+            choice.append((address.id, address))
+        super(OrderAddressForm, self).__init__(*args, **kwargs)
+        self.fields['send_address'].widget.choices = choice
+        self.fields['bill_address'].widget.choices = choice
